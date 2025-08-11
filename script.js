0524cb81e7e1ff371be251bb19ff2e680a0da601
huongngh <info.huonghuynh@users.noreply.github.com>
2025-08-11 10:54:48 +0700

    Create skeleton files

---
+++ script.js
@@ -0,0 +1,49 @@
+const { createElement } = require("react");
+
+const myLibrary = [];
+
+function Book(id, title, author, read) {
+  // the constructor...
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+  const newBook = new Book(id, title, author, read);
+  myLibrary.push(newBook);
+  return newBook;
+}
+
+function displayBook(){
+  const tableBody = document.getElementById('libraryBody');
+  let tableContent = '';
+
+  myLibrary.forEach(book => {
+    //create row
+    const row = document.createElement('tr');
+    
+    //create cell and append to table
+    const idCell = document.createElement('td');
+    idCell.textContent = book.id;
+    row.appendChild(idCell);
+
+    const titleCell = document.createElement('td');
+    titleCell.textContent = book.title;
+    row.appendChild(titleCell);
+
+    const authorCell = document.createElement('td');
+    authorCell.textContent = book.author;
+    row.appendChild(authorCell);
+
+    const readCell = document.createElement('td');
+    readCell.textContent = book.read ? 'Yes' : 'No';
+    row.appendChild(readCell);
+    
+    //append row to table body
+    tableBody.appendChild(row);
+  });
+}
+
